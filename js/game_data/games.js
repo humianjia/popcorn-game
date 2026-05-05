7e3b9f6cfd5b35ca42e1612df618808bc453748b
mianjia.hu <humianjia1995@163.com>
2026-05-05 18:06:49 +0800

    Update all files with popcorn game branding

---
+++ js/game_data/games.js
@@ -0,0 +1,44 @@
+var gamesData = [
+    {
+        "id": "popcorn game",
+        "name": "Popcorn Game",
+        "imageUrl": "img/icon/popcornGame.jpg",
+        "gameType": "Index",
+        "rating": "4.9",
+        "description": "Popcorn Game is your ultimate destination for free online games! Enjoy a wide variety of fun browser games including action, shooter, multiplayer, battle royale, and sniper games. No download required - play instantly!",
+        "keywords": "popcorn game, free online games, browser games, action games, shooter games, multiplayer games, battle royale, sniper games, io games, casual games, fun games, free games, pop corn game",
+        "link": "index.html",
+        "tags": [
+            "popcorn game",
+            "action",
+            "shooter",
+            "multiplayer",
+            "browser game",
+            "io games",
+            "battle royale",
+            "sniper games",
+            "online game",
+            "free game",
+            "casual games",
+            "fun games"
+        ],
+        "iframeUrl": "https://html5.gamedistribution.com/4edfa0f45a154c72adeba5018c999f14/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F",
+        "gameplay": "Welcome to Popcorn Game! Browse our collection of free online games and start playing instantly. From action-packed shooters to strategic multiplayer battles, we have games for every taste. Simply click and play - no downloads required!",
+        "gameModes": [
+            "Single Player",
+            "Multiplayer",
+            "Co-op",
+            "Competitive"
+        ],
+        "features": [
+            "No download required",
+            "Browser-based gaming",
+            "Thousands of free games",
+            "Action, shooter, strategy and more",
+            "Multiplayer modes",
+            "Regular new games added",
+            "Mobile-friendly",
+            "Free to play"
+        ]
+    },
+]
